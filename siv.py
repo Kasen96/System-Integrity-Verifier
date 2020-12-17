b50116bd6c726567641095f065f25a015feb7822
Kasen <CHNZhukc@outlook.com>
2020-12-17 21:56:06 +0800

    update

--- siv.py
+++ siv.py
@@ -1,14 +1,25 @@
-# System Integrity Verifier(SIV)
+#!/usr/bin/env python3
+
+# A System Integrity Verifier(SIV)
 
 import argparse
 
-parser = argparse.ArgumentParser(description="A simple system integrity verifier.")
+description_text = "A simple system integrity verifier."
+example_text = '''Example 1: Initialization mode
+siv -i -D important_directory -V verificationDB -R my_report.txt -H sha1
+Example 2: Verification mode
+siv -v -D important_directory -V verificationDB -R my_report2.txt'''
 
-parser.add_argument('-i', help='initialization mode')
-parser.add_argument('-v', help='verification mode')
-parser.add_argument('-D', help="specify the monitored directory")
-parser.add_argument('-V', help="specify the verification file")
-parser.add_argument('-R', help="specify the report file")
-parser.add_argument('-H', help="specify the hash function")
+parser = argparse.ArgumentParser(prog='siv',
+                                 description=description_text,
+                                 epilog=example_text,
+                                 formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('-i', help='use the initialization mode')
+parser.add_argument('-v', help='use the verification mode')
+parser.add_argument('-D', help="specify the path of the monitored directory")
+parser.add_argument('-V', help="specify the path of the verification file")
+parser.add_argument('-R', help="specify the path of the report file")
+parser.add_argument('-H', help="specify the hash function, 'sha1' or 'md5'")
 
 args = parser.parse_args()
