01c80fe3b26f95a57ec208f3c13eb69c8fbaf250
Kasen <CHNZhukc@outlook.com>
2020-12-19 15:16:21 +0800

    完成 parser

--- siv.py
+++ siv.py
@@ -20,24 +20,33 @@ group = parser.add_mutually_exclusive_group(required=True)  # at least one param
 group.add_argument('-i', dest="mode", action="store_const", const='i', help="use the initialization mode")
 group.add_argument('-v', dest="mode", action="store_const", const='v', help="use the verification mode")
 
-parser.add_argument('-D', dest="monitored_dir", metavar="monitored_directory", required=True,
+parser.add_argument('-D', dest="monitored_dir", metavar="monitored_directory", nargs=1, required=True,
                     help="specify the path of the monitored directory")
-parser.add_argument('-V', dest="verification_file", metavar="verification_file", required=True,
+parser.add_argument('-V', dest="verification_file", metavar="verification_file", nargs=1, required=True,
                     help="specify the path of the verification file")
-parser.add_argument('-R', dest="report_file", metavar="report_file", required=True,
+parser.add_argument('-R', dest="report_file", metavar="report_file", nargs=1, required=True,
                     help="specify the path of the report file")
-parser.add_argument('-H', dest="hash_fuc", choices=['sha1', 'md5'], help="specify the hash function")
+parser.add_argument('-H', dest="hash_fuc", nargs=1, choices=['sha1', 'md5'], help="specify the hash function")
 
 args = parser.parse_args()
 
 mode = args.mode
-monitored_dir = args.monitored_dir
-verification_file = args.verification_file
-report_file = args.report_file
-hash_fuc = args.hash_fuc
+monitored_dir = args.monitored_dir[0]
+verification_file = args.verification_file[0]
+report_file = args.report_file[0]
+hash_fuc = args.hash_fuc[0]
 
-print(mode)
-print(monitored_dir)
-print(verification_file)
-print(report_file)
-print(hash_fuc)
+if mode == 'i':  # initialization mode
+    print("initialization mode")
+    print(f"the monitored dir is: {monitored_dir}")
+    print(f"the verification file is: {verification_file}")
+    print(f"the report file is: {report_file}")
+    print(f"the hash function is: {hash_fuc}")
+else:  # 'v' verification mode
+    if hash_fuc is not None:
+        parser.error("The hash function('-H') can not be used in the verification mode")
+    else:
+        print("verification mode")
+        print(f"the monitored dir is: {monitored_dir}")
+        print(f"the verification file is: {verification_file}")
+        print(f"the report file is: {report_file}")
